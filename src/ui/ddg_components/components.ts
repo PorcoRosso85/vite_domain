48eb2950b8c5e99eaa97f881bdaf768361c0a90f
PorcoRosso85 <1.is.universe@gmail.com>
2023-08-14 18:00:02 +0900

    able to show esample, then try others

--- src/ui/ddg_components/components.ts
+++ src/ui/ddg_components/components.ts
@@ -6,13 +6,21 @@ export class GraphComponent extends HTMLElement {
   constructor() {
     super();
     const shadow = this.attachShadow({ mode: "open" });
+    const slot = document.createElement('slot')
+    shadow.appendChild(slot);
+
     this.container = document.createElement("div");
     this.container.style.width = "100%";
     this.container.style.height = "600px";
-    shadow.appendChild(this.container);
+    // shadow.appendChild(this.container);
   }
 
   connectedCallback() {
+    this.appendChild(this.container);
     initializeGraph(this.container);
+  }
+  
+  disconnectedCallback() {
+    this.removeChild(this.container);
   }
 }
