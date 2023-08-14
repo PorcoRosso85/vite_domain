86637edc47487891de80300ea85629a911a4ce6f
PorcoRosso85 <1.is.universe@gmail.com>
2023-08-14 12:30:31 +0900

    init

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+// vite.config.ts
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  define: {
+    'import.meta.vitest': false,
+  },
+  test: {
+    includeSource: ['src/**/*.{js,ts}'],
+  },
+})
