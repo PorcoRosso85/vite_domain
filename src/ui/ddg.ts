22901d2919e42dc1281bd384a6c8b8daf9df337c
PorcoRosso85 <1.is.universe@gmail.com>
2023-08-14 18:03:56 +0900

    refactor where to component

--- src/ui/ddg.ts
+++ src/ui/ddg.ts
@@ -1,4 +1,4 @@
-import { GraphComponent } from "./ddg_components/components";
+import { initializeGraph } from "./ddg_components/initializeGraph"
 
 // CustomElementの定義
 class ComponentOne extends HTMLElement {
@@ -29,6 +29,32 @@ class ComponentTwo extends HTMLElement {
     container.style.padding = "10px";
 
     shadow.appendChild(container);
+  }
+}
+
+
+export class GraphComponent extends HTMLElement {
+  private container: HTMLDivElement;
+
+  constructor() {
+    super();
+    const shadow = this.attachShadow({ mode: "open" });
+    const slot = document.createElement('slot')
+    shadow.appendChild(slot);
+
+    this.container = document.createElement("div");
+    this.container.style.width = "100%";
+    this.container.style.height = "600px";
+    // shadow.appendChild(this.container);
+  }
+
+  connectedCallback() {
+    this.appendChild(this.container);
+    initializeGraph(this.container);
+  }
+  
+  disconnectedCallback() {
+    this.removeChild(this.container);
   }
 }
 
