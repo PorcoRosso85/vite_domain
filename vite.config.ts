a80b536d9cf602d22be941e63b6e87d341f7c2e5
PorcoRosso85 <1.is.universe@gmail.com>
2023-08-15 13:35:39 +0900

    fix vite, vitest setting

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,18 @@
-// vite.config.ts
-// import { defineConfig } from 'vitest/config'
-
-// export default defineConfig({
-//   define: {
-//     'import.meta.vitest': false,
-//   },
-//   test: {
-//     globals: true, 
-//     includeSource: ['src/**/*.{js,ts}'],
-//   },
-//   root: 'src/ui',
-//   // base: '/ui',
-//   publicDir: 'public',
-//   build: {
-//     outDir: '../../dist',
-//   },
-//   envDir: '../../',
-// })
-// 
-
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  root: 'src/ui',
   plugins: [tsconfigPaths()],
+
+  build: {
+    outDir: './dist',
+  },
+  define: {
+    'import.meta.vitest': false,
+  },
+  test: {
+    globals: true, 
+    includeSource: ['src/**/*.{js,ts}'],
+  },
 })
