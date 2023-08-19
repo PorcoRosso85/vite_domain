cda28911174a94f937dfed752b99caf12fcbb3ca
PorcoRosso85 <1.is.universe@gmail.com>
2023-08-19 10:31:25 +0900

    update config and dependency

--- src/infrastructure/config/config.ts
+++ src/infrastructure/config/config.ts
@@ -1,12 +1,31 @@
+import Logger from 'domain/logs/logs';
+
 namespace Environment {
-  export const EnvironmentPhases = {
-    DEV: 'dev',
-    STG: 'stg',
-    PRD: 'prd',
+  export const EnvPhases = {
+    DEV: 'development',
+    STG: 'staging',
+    PRD: 'production',
   } as const;
 
-  export type EnvironmentPhase =
-    (typeof EnvironmentPhases)[keyof typeof EnvironmentPhases];
+  export type EnvPhase = (typeof EnvPhases)[keyof typeof EnvPhases];
+
+  export const EnvVerLibraries = {
+    [Environment.EnvPhases.DEV]: {
+      DATABASE_URL: 'DEV_DATABASE_URL',
+      API_URL: 'DEV_API_URL',
+    },
+    [Environment.EnvPhases.STG]: {
+      DATABASE_URL: 'STG_DATABASE_URL',
+      API_URL: 'STG_API_URL',
+    },
+    [Environment.EnvPhases.PRD]: {
+      DATABASE_URL: 'PRD_DATABASE_URL',
+      API_URL: 'PRD_API_URL',
+    },
+  } as const;
+
+  export type EnvVarLibrary =
+    (typeof EnvVerLibraries)[keyof typeof EnvVerLibraries];
 }
 
 interface ConfigProvider {
@@ -23,30 +42,134 @@ class EnvConfigProvider implements ConfigProvider {
   }
 }
 
-// このサンプルでは、EnvConfigProviderのみを使用していますが、
-// CloudConfigProviderなどを追加することも可能です。
+import { SSM } from 'aws-sdk';
 class CloudConfigProvider implements ConfigProvider {
-  // ここでは具体的な実装を省略していますが、クラウドサービスのAPIを利用して設定を取得するロジックが入ります
-  get(key: string): string | undefined {
-    // AWSのParameter Storeから取得するなどの実際の取得ロジック
-    // return ...;
+  private ssm: SSM;
+
+  constructor() {
+    this.ssm = new SSM();
+  }
+
+  async get(key: string): Promise<string | undefined> {
+    try {
+      const result = await this.ssm
+        .getParameter({
+          Name: key,
+          WithDecryption: true,
+        })
+        .promise();
+
+      return result.Parameter?.Value;
+    } catch (error) {
+      Logger.error(
+        `Error fetching parameter ${key} from AWS Parameter Store:`,
+        error,
+      );
+      return undefined;
+    }
   }
 }
 
-function createConfigProvider(): ConfigProvider {
-  const env = process.env.NODE_ENV || Environment.EnvironmentPhases.DEV;
-  if (env === Environment.EnvironmentPhases.PRD) {
-    return new CloudConfigProvider();
+// APP_ENVのバリデーション
+function isValidEnvPhase(value: string): value is Environment.EnvPhase {
+  return Object.values(Environment.EnvPhases).includes(
+    value as Environment.EnvPhase,
+  );
+}
+function createConfigProvider(): [ConfigProvider, Environment.EnvPhase] {
+  const processAppEnv = process.env.APP_ENV || Environment.EnvPhases.DEV;
+
+  // APP_ENVのバリデーション
+  if (!isValidEnvPhase(processAppEnv)) {
+    throw new Error(`Invalid APP_ENV value: ${processAppEnv}`);
+  }
+
+  if (processAppEnv === Environment.EnvPhases.PRD) {
+    return [new CloudConfigProvider(), processAppEnv];
   } else {
-    return new EnvConfigProvider();
+    return [new EnvConfigProvider(), processAppEnv];
   }
 }
 
-const configProvider = createConfigProvider();
+const [configProvider, processAppEnv] = createConfigProvider();
 
-// 環境に応じて異なる環境変数を取得する。
-const env = process.env.NODE_ENV || Environment.EnvironmentPhases.DEV;
-const DATABASE_URL = configProvider.get(`${env.toUpperCase()}_DATABASE_URL`);
-const API_URL = configProvider.get(`${env.toUpperCase()}_API_URL`);
+const DATABASE_URL = configProvider.get(
+  Environment.EnvVerLibraries[processAppEnv].DATABASE_URL,
+);
+const API_URL = configProvider.get(
+  Environment.EnvVerLibraries[processAppEnv].API_URL,
+);
 
 export { DATABASE_URL, API_URL };
+
+if (import.meta.vitest) {
+  const { describe, it, expect } = import.meta.vitest;
+
+  describe('環境設定のテスト', () => {
+    describe('isValiedEnvPhase関数のテスト', () => {
+      it('APP_ENVバリューが正しければtrueを返す', () => {
+        expect(isValidEnvPhase('development')).toBe(true);
+        expect(isValidEnvPhase('staging')).toBe(true);
+        expect(isValidEnvPhase('production')).toBe(true);
+      });
+
+      it('APP_ENVバリューが誤っていればfalseを返す', () => {
+        expect(isValidEnvPhase('invalid_env')).toBe(false);
+      });
+    });
+
+    describe('createConfigProvider関数のテスト', () => {
+      // 一時的にprocess.env.APP_ENVをモックするためのヘルパー関数
+      function mockAppEnv(value: string) {
+        process.env.APP_ENV = value;
+      }
+
+      it('DEV・STG環境ならEnvConfigProviderを返す', () => {
+        mockAppEnv('development');
+        let [provider, env] = createConfigProvider();
+        expect(provider instanceof EnvConfigProvider).toBe(true);
+        expect(env).toBe('development');
+
+        mockAppEnv('staging');
+        [provider, env] = createConfigProvider();
+        expect(provider instanceof EnvConfigProvider).toBe(true);
+        expect(env).toBe('staging');
+      });
+
+      it('PRD環境ならCloudConfigProviderを返す', () => {
+        mockAppEnv('production');
+        const [provider, env] = createConfigProvider();
+        expect(provider instanceof CloudConfigProvider).toBe(true);
+        expect(env).toBe('production');
+      });
+
+      it('不正なAPP_ENVバリューだとエラーを返す', () => {
+        mockAppEnv('invalid');
+        expect(() => createConfigProvider()).toThrowError(
+          'Invalid APP_ENV value: invalid',
+        );
+      });
+    });
+
+    describe('ConfigProviderの動作テスト', () => {
+      it('EnvConfigProviderが.envから設定を取得する', () => {
+        const provider = new EnvConfigProvider();
+        expect(provider.get('TEST_KEY')).toBe('test_key_sample');
+      });
+
+      // // TODO: 本番環境のテスト
+      // function mockSsmGetParameter(response: any) {
+      //   // import.meta.vitestのモック機能
+      // }
+
+      // it('CloutConfigProviderがクラウドサービスから設定を取得する', async () => {
+      //   const expectedValue = 'mock_value_from_aws';
+      //   mockSsmGetParameter({ Parameter: { Value: expectedValue } });
+
+      //   const provider = new CloudConfigProvider();
+      //   const gotValue = await provider.get('TEST_KEY_MOCK');
+      //   expect(gotValue).toBe(expectedValue);
+      // });
+    });
+  });
+}
