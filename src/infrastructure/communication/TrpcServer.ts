3a13db4a751a2dece4472fdedd50cf2934ad7a3f
PorcoRosso85 <1.is.universe@gmail.com>
2023-09-05 20:13:08 +0900

    trpc

--- src/infrastructure/communication/TrpcServer.ts
+++ src/infrastructure/communication/TrpcServer.ts
@@ -1,8 +1,16 @@
 import { initTRPC } from '@trpc/server';
 import { createHTTPServer } from '@trpc/server/adapters/standalone';
 
+/**
+ * Initialization of tRPC backend
+ * Should be done only once per backend!
+ */
 const t = initTRPC.create();
 
+/**
+ * Export reusable router and procedure helpers
+ * that can be used throughout the router
+ */
 export const router = t.router;
 export const publicProcedure = t.procedure;
 
